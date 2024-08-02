ff0764dc70c5aa4300f219774088dd98f79424dd
bgates747 <brandon.gates@gmail.com>
2024-08-01 22:33:50 -0400

    matrix multiplication using dspm_mult_f32

--- video/pingo/math/mat3.c
+++ video/pingo/math/mat3.c
@@ -3,6 +3,7 @@
 
 #include <math.h>
 #include <stdint.h>
+#include <dspm_mult.h>
 
 Mat3 mat3Identity() {
     return (Mat3){{
@@ -49,19 +50,25 @@ Vec2f mat3Multiply(Vec2f *v, Mat3 *t) {
     return (Vec2f){a,b};
 }
 
-Mat3 mat3MultiplyM( Mat3 * m1, Mat3 * m2) {
+// Mat3 mat3MultiplyM( Mat3 * m1, Mat3 * m2) {
+//     Mat3 out;
+//     F_TYPE * a = m2->elements;
+//     F_TYPE * b = m1->elements;
+//     out.elements[0] = a[0] * b[0] + a[1] * b[3] + a[2] * b[6];
+//     out.elements[1] = a[0] * b[1] + a[1] * b[4] + a[2] * b[7];
+//     out.elements[2] = a[0] * b[2] + a[1] * b[5] + a[2] * b[8];
+//     out.elements[3] = a[3] * b[0] + a[4] * b[3] + a[5] * b[6];
+//     out.elements[4] = a[3] * b[1] + a[4] * b[4] + a[5] * b[7];
+//     out.elements[5] = a[3] * b[2] + a[4] * b[5] + a[5] * b[8];
+//     out.elements[6] = a[6] * b[0] + a[7] * b[3] + a[8] * b[6];
+//     out.elements[7] = a[6] * b[1] + a[7] * b[4] + a[8] * b[7];
+//     out.elements[8] = a[6] * b[2] + a[7] * b[5] + a[8] * b[8];
+//     return out;
+// }
+
+Mat3 mat3MultiplyM(Mat3 *m1, Mat3 *m2) {
     Mat3 out;
-    F_TYPE * a = m2->elements;
-    F_TYPE * b = m1->elements;
-    out.elements[0] = a[0] * b[0] + a[1] * b[3] + a[2] * b[6];
-    out.elements[1] = a[0] * b[1] + a[1] * b[4] + a[2] * b[7];
-    out.elements[2] = a[0] * b[2] + a[1] * b[5] + a[2] * b[8];
-    out.elements[3] = a[3] * b[0] + a[4] * b[3] + a[5] * b[6];
-    out.elements[4] = a[3] * b[1] + a[4] * b[4] + a[5] * b[7];
-    out.elements[5] = a[3] * b[2] + a[4] * b[5] + a[5] * b[8];
-    out.elements[6] = a[6] * b[0] + a[7] * b[3] + a[8] * b[6];
-    out.elements[7] = a[6] * b[1] + a[7] * b[4] + a[8] * b[7];
-    out.elements[8] = a[6] * b[2] + a[7] * b[5] + a[8] * b[8];
+    dspm_mult_f32(m1->elements, m2->elements, out.elements, 3, 3, 3);
     return out;
 }
 
